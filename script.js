a07cf7fb22679cf748bd465663ca8958723e76cb
sarunas slekys <sarunasslekys91@gmail.com>
2023-05-26 22:33:49 +0100

    last-steps

---
+++ script.js
@@ -0,0 +1,26 @@
+let map, infoWindow;
+
+function initMap(){
+  
+    const myLatLng = {lat: 51.19517018673943, lng: -3.4657533019415023};
+
+    const map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 15,
+        center: {lat: 51.19517018673943, lng: -3.4657533019415023},
+        mapTypeId: 'satellite',
+    });
+   
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      
+    });
+  }
+    infoWindow = new google.maps.InfoWindow();
+
+    
+
+    
+
+
+window.initMap = initMap;
